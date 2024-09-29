6dec0c3cbfd0d3e808cf8504c7dc7c30757b13e9
merve <merveaydin86@hotmail.com>
2024-09-29 21:37:26 +0300

    first commit

---
+++ src/helpers/data.jsx
@@ -0,0 +1,75 @@
+export const appointmentData = [
+  {
+    id: 1,
+    patient: "Timmy Donaldson",
+    day: new Date(),
+    consulted: true,
+    doctor: "Prof. Akari Yoshino",
+  },
+  {
+    id: 2,
+    patient: "Bobbie Martel",
+    day: new Date(),
+    consulted: false,
+    doctor: "Prof. Dr. Theodor Jackson",
+  },
+  {
+    id: 3,
+    patient: "Elisa Daniel",
+    day: new Date(),
+    consulted: true,
+    doctor: "Dr. Jasmine Carpanter",
+  },
+];
+
+export const carData = [
+  {
+    id: 1,
+    name: "BMW",
+    type: "Diesel",
+    img: "./img/bmw.jpg",
+  },
+  {
+    id: 2,
+    name: "Mercedes-Benz",
+    type: "Petrol",
+    img: "./img/mercedes-benz.png",
+  },
+  {
+    id: 3,
+    name: "Audi",
+    type: "Electric",
+    img: "./img/audi.png",
+  },
+  {
+    id: 4,
+    name: "Tesla",
+    type: "Electric",
+    img: "./img/tesla.webp",
+  },
+  {
+    id: 5,
+    name: "Ford Mustang",
+    type: "Petrol",
+    img: "./img/ford-mustang.jpg",
+  },
+  {
+    id: 6,
+    name: "Volkswagen Golf",
+    type: "Diesel",
+    img: "./img/volkswagen-golf.webp",
+  },
+  {
+    id: 7,
+    name: "Jeep Wrangler",
+    type: "Petrol",
+    img: "./img/Jeep Wrangler.jpeg",
+  },
+  {
+    id: 8,
+    name: "Range Rover",
+    type: "Diesel",
+    img: "./img/Range Rover.jpg",
+  },
+];
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Cars from "../components/Cars"
+import ReservationList from "../components/ReservationList"
+
+
+const Home = () => {
+  return (
+    <main className="text-center mt-2">
+      <h1 className="display-5 text-danger">RANT A CAR</h1>
+      <Cars />
+      <ReservationList />    
+    </main>
+  )
+}
+
+export default Home
